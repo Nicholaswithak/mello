45cd0cb3b703bb9622d15fa4f66445b99552706e
Nicholas Lee <ncl1984@gmail.com>
2020-01-03 04:57:07 +0000

    finished mello javascript

--- public/assets/js/board.js
+++ public/assets/js/board.js
@@ -36,7 +36,9 @@ function getBoard(id) {
       renderBoard();
     })
     .catch(function(err) {
-      location.replace('/boards');
+      if (err.statusText === 'Unauthorized') {
+        location.replace('/boards');
+      }
     });
 }
 
@@ -117,13 +119,38 @@ function renderBoard() {
 
 function renderContributors() {
   let $contributorListItems = board.users.map(function(user) {
-    let $contributorListItem = $('<li>').text(user.email);
+    let $contributorListItem = $('<li>');
+    let $contributorSpan = $('<span>').text(user.email);
+    let $contributorDeleteButton = $(
+      '<button class="danger">Remove</button>')
+    .data(user)
+    .on('click', handleContributorDelete);
+
+    $contributorListItem.append($contributorSpan, $contributorDeleteButton);
+
     return $contributorListItem;
   });
 
   $contributorModalList.empty();
   $contributorModalList.append($contributorListItems);
 }
+
+function handleContributorDelete(event) {
+  let { id, email } = $(event.target).data();
+
+  $.ajax({
+    url: '/api/user_boards',
+    method: 'DELETE',
+    data: {
+      user_id: id,
+      board_id: board.id
+    }
+  }).then(function() {
+    init();
+    displayMessage(`Successfully removed user: ${email}`, 'success');
+  });
+}
+
 
 function makeSortable() {
   Sortable.create($boardContainer[0], {
